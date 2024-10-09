8038aa2ac3340d4fc309000e09271332912defcd
Plamen Gopsodinov <mr.gospodinov@gmail.com>
2024-10-09 17:58:48 +0300

    Working cucumber test runner

--- cucumber.js
+++ cucumber.js
@@ -1,11 +1,49 @@
-const common = `
-  --parallel 2
-  --format summary
-  --format progress-bar
-  --format-options ${JSON.stringify({ snippetInterface: "async-await" })}
-  --publish-quiet
-`;
+const fs = require('fs');
+const path = require('path');
+const resultsDir = path.join(__dirname, 'output/results');
+
+/**
+ * check if e2e/results dir exists, if not create
+ */
+if (!fs.existsSync(resultsDir)) {
+  fs.mkdirSync(resultsDir);
+}
+
+/**
+ * Cucumber.js Config
+ * Cucumber profiles for different browser / devices
+ */
+
+// Node parameters set via command line or CI
+const common = {
+  format: [
+    `json:./e2e/results/cucumber-report.json`,
+    '@cucumber/pretty-formatter',
+    'html:./e2e/results/cucumber-report.html',
+  ],
+  requireModule: ['ts-node/register'],
+  require: [path.join(__dirname, 'src/**/*.ts')],
+  paths: [path.join(__dirname, 'src/features/')],
+  timeout: 30000,
+  worldParameters: {
+    headless: false,
+    appUrl: process.env.APP_URL || 'http://localhost:4200',
+    mockApiUrl: process.env.APP_URL || 'http://apimock:3000',
+  },
+};
+
+const local = {
+  ...common,
+};
+
+
+const debug = {
+  ...common,
+  mode: 'generate', // cucumber option for generating reference screenshots
+  paths: [], // set to empty as path defined in launch.json feature debug
+};
 
 module.exports = {
-  default: `${common}`,
+  default: local,
+  debug: debug,
 };
--- src/features/google.feature
+++ src/features/google.feature
@@ -1,4 +1,5 @@
 Feature: Open Google
 
+  @run  
   Scenario: Open Google homepage
     Given I open Google homepage
